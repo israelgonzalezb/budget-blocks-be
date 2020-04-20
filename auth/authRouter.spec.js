4e2d5f38e39ee8440839f12025d947cb9a988b23
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-04-20 14:18:46 -0700

    Adding and updating registration and login tests
    Co-authored-by: Trevor Martin <trevor.j.martin@gmail.com>

--- auth/authRouter.spec.js
+++ auth/authRouter.spec.js
@@ -2,36 +2,122 @@ const request = require("supertest");
 const server = require("../data/server");
 const Users = require("../users/users-model");
 const db = require("../data/db-config");
-
-describe("Users Login & Register", () => {
-
-  let newUserID
-
-  beforeAll(async () => {
-
+/*
+describe("000: Auth-Router GET", function () {
+  // beforeEach(() => {
+  //   moxios.install(request);
+  //   jest.clearAllMocks();
+  // });
+  afterAll(async () => {
+    // avoid jest open handle error
+    await new Promise((resolve) => setTimeout(() => resolve(), 500));
   });
+  test("should return 404 on GET /api/auth/register", async () => {
+    const result = await app.get("/api/auth/register");
+    expect(result.status).toBe(401);
+  });
+  test("should return 404 on GET /api/auth/login", async () => {
+    const result = await app.get("/api/auth/login");
+    expect(result.status).toBe(401);
+  });
+});
+*/
+
+describe("Login and Registration", () => {
+  describe("Registration", () => {
+    describe("Check validity of HTTP methods", () => {
+      test("should return 404 on GET /api/auth/register", async () => {
+        const result = await request(server).get("/api/auth/register");
+        expect(result.status).toBe(404);
+      });
+      test("should return 404 on PUT /api/auth/register", async () => {
+        const result = await request(server).put("/api/auth/register").send({});
+        expect(result.status).toBe(404);
+      });
+      test("should return 404 on DELETE /api/auth/register", async () => {
+        const result = await request(server)
+          .delete("/api/auth/register")
+          .send({ id: 1 });
+        expect(result.status).toBe(404);
+      });
+    });
+    describe("Registers a new user", () => {
+      beforeAll(async () => {
+        process.env.NODE_ENVIRONMENT == "test" &&
+          (await db.raw("truncate table users cascade"));
+        await db("users");
+      });
+
+      const user = {
+        email: "test@test.com",
+        password: "password",
+        first_name: "greg", // Seems to be optional?
+        last_name: "lala", // Seems to be optional?
+      };
+
+      test("Should allow a new user to be created", async () => {
+        const response = await request(server)
+          .post("/api/auth/register")
+          .send(user);
+        expect(response.status).toBe(201);
+      });
+
+      test("Should not allow you to register a duplicate email address", async () => {
+        // 403 Forbidden http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.4
+        const response = await request(server)
+          .post("/api/auth/register")
+          .send(user);
+        expect(response.status).toBe(400);
+      });
+    });
+    // describe("Login", () => {
+
+    // })
+  });
+});
+
+/*
+describe("Users Login & Register", () => {
+  let newUserID;
+
+  beforeAll(async () => {});
 
   afterAll(async () => {
-    await Users.deleteUser(newUserID)
+    await Users.deleteUser(newUserID);
   });
 
-  describe("Logs user in", () => {
+  describe("Registers a new user", () => {
     beforeAll(async () => {
-      await db("users")
+      await db("users");
     });
 
     test("Should allow a new user to be created", async () => {
-      // Registers a new user;
-
-      const user = { email: "test@test.com", password: "password", first_name: "greg", last_name: "lala" };
+      const user = {
+        email: "test@test.com",
+        password: "password",
+        first_name: "greg", // Seems to be optional?
+        last_name: "lala", // Seems to be optional?
+      };
       const response = await request(server)
         .post("/api/auth/register")
         .send(user);
-
-      console.log('userCreate', response.body, response.status)
-
-
       expect(response.status).toBe(201);
+      console.log("register response", response);
+      await Users.deleteUser(response.body.id);
+    });
+
+    test("Should not allow you to register a duplicate email address", async () => {
+      const user = {
+        email: "test@test.com",
+        password: "password",
+        first_name: "greg", // Seems to be optional?
+        last_name: "lala", // Seems to be optional?
+      };
+      // 403 Forbidden http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.4
+      const response = await request(server)
+        .post("/api/auth/register")
+        .send(user);
+      expect(response.status).toBe(403);
     });
   });
 
@@ -39,16 +125,14 @@ describe("Users Login & Register", () => {
     const user = { email: "test@test.com", password: "password" };
 
     // await Users.login(user);
-    const response = await request(server)
-      .post("/api/auth/login")
-      .send(user);
+    const response = await request(server).post("/api/auth/login").send(user);
 
-    console.log('allowLogin', response.body, response.status);
+    // console.log("allowLogin", response.body, response.status);
 
     expect(response.status).toBe(200);
     expect(response.body).toBeDefined();
 
-    newUserID = response.body.id
+    newUserID = response.body.id;
   });
 
   test("Should return an error if no body is passed in", async () => {
@@ -71,9 +155,7 @@ describe("Users Login & Register", () => {
     const user = { password: "password" };
 
     // await Users.login(user);
-    const response = await request(server)
-      .post("/api/auth/login")
-      .send(user);
+    const response = await request(server).post("/api/auth/login").send(user);
     // console.log(response);
 
     expect(response.status).toBe(400);
@@ -85,9 +167,7 @@ describe("Users Login & Register", () => {
     const user = { email: "test@test.com" };
 
     // await Users.login(user);
-    const response = await request(server)
-      .post("/api/auth/login")
-      .send(user);
+    const response = await request(server).post("/api/auth/login").send(user);
     // console.log(response);
 
     expect(response.status).toBe(400);
@@ -108,5 +188,5 @@ describe("Users Login & Register", () => {
 
     expect(response).toBeDefined();
   });
-
 });
+*/
